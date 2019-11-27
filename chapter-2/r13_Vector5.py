4c918e8cf697475a11bb78bd52cef753810f8950
Adam Matheson <adam.matheson@ntlworld.com>
2019-11-27 22:57:29 +0000

    CH2 Reinforcement 9-14

---
+++ chapter-2/r13_Vector5.py
@@ -0,0 +1,95 @@
+class Vector:
+  """Represent a vector in a multidimensional space."""
+
+  def __init__(self, d):
+    """Create d-dimensional vector of zeros."""
+    self._coords = [0] * d
+
+  def __len__(self):
+    """Return the dimension of the vector."""
+    return len(self._coords)
+
+  def __getitem__(self, j):
+    """Return jth coordinate of vector."""
+    return self._coords[j]
+
+  def __setitem__(self, j, val):
+    """Set jth coordinate of vector to given value."""
+    self._coords[j] = val
+
+  def __add__(self, other):
+    """Return sum of two vectors."""
+    if len(self) != len(other):
+      raise ValueError('dimensions must agree')
+    result = Vector(len(self))  # start with a vector of zeros
+    for j in range(len(self)):
+      result[j] = self[j] + other[j]
+    return result
+
+  def __radd__(self, other):
+    """Return sum of two vectors."""
+    if len(self) != len(other):
+      raise ValueError('dimensions must agree')
+    result = Vector(len(self))  # start with a vector of zeros
+    for j in range(len(self)):
+      result[j] = self[j] + other[j]
+    return result
+
+  def __sub__(self, other):
+    """Return subtraction of two vectors."""
+    if len(self) != len(other):
+      raise ValueError('dimensions must agree')
+    result = Vector(len(self))  # start with a vector of zeros
+    for j in range(len(self)):
+      result[j] = self[j] - other[j]
+    return result
+
+  def __mul__(self, other):
+    """Return product of two vectors."""
+    result = Vector(len(self))  # start with a vector of zeros
+    for j in range(len(self)):
+      result[j] = self[j] * other
+    return result
+
+  __rmul__ = __mul__
+
+  def __eq__(self, other):
+    """Return True if vector has same coordinates as other."""
+    return self._coords == other._coords
+
+  def __ne__(self, other):
+    """Return True if vector differs from other."""
+    return not self == other
+
+  def __neg__(self):
+    """Return a new vector whose coordinates are the negated values of the original coordinates."""
+    result = Vector(len(self))  # start with a vector of zeros
+    for j in range(len(self)):
+      result[j] = -self[j]
+    return result
+
+  def __str__(self):
+    """Produce string representation of vector."""
+    return '<' + str(self._coords)[1:-1] + '>'
+
+
+test1 = Vector(3)
+test2 = Vector(3)
+
+test1.__setitem__(0, 2)
+test1.__setitem__(1, 3)
+test1.__setitem__(2, 4)
+test2.__setitem__(0, 5)
+test2.__setitem__(1, 6)
+test2.__setitem__(2, 7)
+
+print(test1, test2)
+print(test2 - test1)
+
+print(-test1)
+
+print(test1 + [1, 1, 1])
+print([1, 1, 1] + test1)
+
+print(test1 * 3)
+print(3 * test1)
